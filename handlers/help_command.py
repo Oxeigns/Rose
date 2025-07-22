4feb0da7ca368f49e76a8c278ae10798cc6d1874
Samrat <yashsamrat10080@gmail.com>
2025-07-22 22:53:46 +0530

    Delete handlers directory

--- handlers/help_command.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.handlers import MessageHandler
-
-from .start import help_cmd
-
-
-def register(app: Client) -> None:
-    """Register the /help command."""
-    app.add_handler(MessageHandler(help_cmd, filters.command("help")))
